027f405a81930dd786761ffebf1e017f486d76a6
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-05-23 11:57:00 +0800

    chore: update

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -7,9 +7,18 @@ import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+import { createI18n } from 'vue-i18n'
 
+const i18n = createI18n({
+  globalInjection: true,
+  locale: 'zh-CN',
+  messages: {
+  },
+  legacy: false
+});
 const app = createApp(App)
 app.use(createPinia())
+app.use(i18n)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
