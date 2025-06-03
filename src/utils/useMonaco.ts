6d8a4785822b2d9856fb805fa6cb4bf2cf20142f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-06-03 19:00:36 +0800

    fix: update vue-i18n dependency version and clean up unused worker imports in useMonaco

--- src/utils/useMonaco.ts
+++ src/utils/useMonaco.ts
@@ -273,3 +273,29 @@ export function useMonaco(monacoOptions: MonacoOptions = {}) {
     },
   }
 }
+
+// import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+// import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+// import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+// import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+// import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+// // @ts-expect-error monaco global is not defined in this context
+// globalThis.MonacoEnvironment = {
+//   getWorker(_: any, label: string) {
+//     if (label === 'json') {
+//       return new JsonWorker()
+//     }
+//     if (label === 'css' || label === 'scss' || label === 'less') {
+//       return new CssWorker()
+//     }
+//     if (label === 'html' || label === 'handlebars' || label === 'razor') {
+//       return new HtmlWorker()
+//     }
+//     if (label === 'typescript' || label === 'javascript') {
+//       return new TsWorker()
+//     }
+//     return new EditorWorker()
+//   },
+// }
+
+// monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
